c30e5c425a70102142e5f4ced6d3e80f44b940fa
AhmedEzzat21 <a7med.ibn.3zzat@gmail.com>
2021-06-07 13:58:28 +0200

    app center configre

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,13 +1,21 @@
 import UIKit
-
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
+    //a8f3727a-0ed3-45df-b27f-5093ed777ca6
     static var entries: FileEntryStore = FileEntryStore()
     
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+      
+
+        AppCenter.start(withAppSecret: "be17a075-6ad8-44af-9bab-58037e555397", services: [Analytics.self, Crashes.self , Distribute.self])
 
         return true
     }
